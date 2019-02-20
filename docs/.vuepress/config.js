29ff20e75bba60cad61bf098a0b3ec9e2de388f3
Christopher Mühl <christopher@padarom.io>
2019-02-20 20:06:37 +0100

    Add testing documentation basis

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,9 @@
+const { resolve } = require('path')
+
 module.exports = {
+    plugins: [
+	    'code-switcher'
+    ],
     title: 'blurple.js',
     description: 'The blurple.js Discord Bot Framework',
     themeConfig: {
@@ -8,27 +13,49 @@ module.exports = {
         editLinks: true,
         editLinkText: 'Edit this page on GitHub',
         lastUpdated: 'Last updated',
-        sidebar: [
-            ['/', 'Start'],
+        nav: [
             {
-                title: 'Runtime',
-                collapsable: false,
-                children: [
-                    '/runtime/installation',
-                    '/runtime/configuration',
-                    '/runtime/usage'
-                ]
+                text: 'Guide',
+                link: '/guide/'
             },
             {
-                title: 'Bot Extensions',
-                collapsable: false,
-                children: [
-                    '/extensions/setup',
-                    '/extensions/api',
-                    '/extensions/testing',
-                    '/extensions/example'
-                ]
+                text: 'Testing',
+                link: '/testing/'
             }
-        ]
+        ],
+        sidebar: {
+            '/guide/': getGuideSidebar(),
+            '/testing/': getTestingSidebar()
+        }
     }
+}
+
+function getGuideSidebar () {
+    return [
+        ['', 'Start'],
+        {
+            title: 'Runtime',
+            collapsable: false,
+            children: [
+                'runtime/installation',
+                'runtime/configuration',
+                'runtime/usage'
+            ]
+        },
+        {
+            title: 'Bot Extensions',
+            collapsable: false,
+            children: [
+                'extensions/setup',
+                'extensions/api',
+                'extensions/example'
+            ]
+        }
+    ]
+}
+
+function getTestingSidebar () {
+    return [
+
+    ]
 }
---
+++ docs/testing/README.md
@@ -0,0 +1,2 @@
+# Testing
+For now, refer to the Readme of the [GitHub repository](https://github.com/blurplejs/testing).
